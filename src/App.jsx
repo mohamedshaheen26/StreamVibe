eb7a3e9e28353a14635cfb1650351ff3d0864e35
Mohamed Shahin <mshahin@innotech.com.eg>
2024-11-07 18:21:54 +0200

    Enhance NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import MovieGrid from "./components/MovieGrid";
 import "./App.css";
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,58 @@
 import React from "react";
-import styles from "./NavBar.module.css";
 
 function Navbar() {
   return (
-    <nav className={styles.Navbar}>
-      <div className={styles.logo}>
-        <img src='src\assets\Vector.png' alt='Logo Stream' />
-        StreamVibe
+    <nav className='navbar navbar-expand-lg'>
+      <div className='container'>
+        <a className='navbar-brand' href='#'>
+          <img
+            src='src/assets/Vector.png'
+            alt='Logo Stream'
+            width='30'
+            height='30'
+          />
+          StreamVibe
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='navbarNav' id='navbarNav'>
+          <ul className='navbar-nav ms-auto'>
+            <li className='nav-item'>
+              <a className='nav-link active' aria-current='page' href='#'>
+                Home
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>
+                Movies & Shows
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>
+                Support
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>
+                Subscriptions
+              </a>
+            </li>
+          </ul>
+        </div>
+        <span class='navbar-text'>
+          <i className='fas fa-search'></i>
+          <i className='fa-regular fa-bell'></i>
+        </span>
       </div>
-      <ul className={styles.navLinks}>
-        <li>Home</li>
-        <li>Movies & Shows</li>
-        <li>Support</li>
-        <li>Subscriptions</li>
-      </ul>
     </nav>
   );
 }
