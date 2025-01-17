7b7758c941f185a401bb965ffe136534dd33a173
Mohamed shaheen <mohamedshaheen1898@gmail.com>
2025-01-18 00:51:27 +0200

    Add pagenation

--- src/pages/GenrePage.jsx
+++ src/pages/GenrePage.jsx
@@ -11,8 +11,8 @@ const GenrePage = () => {
   const [data, setData] = useState([]); // Store movies or shows
   const [genreName, setGenreName] = useState("");
   const [loading, setLoading] = useState(true);
-
-  console.log(id, type);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const fetchGenreName = async () => {
@@ -38,17 +38,60 @@ const GenrePage = () => {
       try {
         // Fetch movies or shows based on the type
         const endpoint = type === "movies" ? "movie" : "tv";
-        const response = await axios.get(
-          `${API_BASE_URL}/discover/${endpoint}`,
-          {
+        const [
+          page1Response,
+          page2Response,
+          page3Response,
+          page4Response,
+          page5Response,
+        ] = await Promise.all([
+          axios.get(`${API_BASE_URL}/discover/${endpoint}`, {
             params: {
               api_key: API_KEY,
               with_genres: id,
+              page: page * 5 - 4, // Fetch the first page of 20 results
             },
-          }
-        );
-        setData(response.data.results);
-        console.log(response);
+          }),
+          axios.get(`${API_BASE_URL}/discover/${endpoint}`, {
+            params: {
+              api_key: API_KEY,
+              with_genres: id,
+              page: page * 5 - 3, // Fetch the second page of 20 results
+            },
+          }),
+          axios.get(`${API_BASE_URL}/discover/${endpoint}`, {
+            params: {
+              api_key: API_KEY,
+              with_genres: id,
+              page: page * 5 - 3, // Fetch the third page of 20 results
+            },
+          }),
+          axios.get(`${API_BASE_URL}/discover/${endpoint}`, {
+            params: {
+              api_key: API_KEY,
+              with_genres: id,
+              page: page * 5 - 1, // Fetch the third page of 20 results
+            },
+          }),
+          axios.get(`${API_BASE_URL}/discover/${endpoint}`, {
+            params: {
+              api_key: API_KEY,
+              with_genres: id,
+              page: page * 5, // Fetch the third page of 20 results
+            },
+          }),
+        ]);
+
+        // Combine the results from all three pages
+        const combinedResults = [
+          ...page1Response.data.results,
+          ...page2Response.data.results,
+          ...page3Response.data.results,
+          ...page4Response.data.results,
+          ...page5Response.data.results,
+        ];
+        setData(combinedResults);
+        setTotalPages(Math.ceil(page1Response.data.total_pages / 5));
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -57,9 +100,23 @@ const GenrePage = () => {
     };
     fetchGenreName();
     fetchData();
-  }, [id, type]);
+  }, [id, type, page]);
+
+  console.log(data);
 
   if (loading) return <p>Loading...</p>;
+
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage((prevPage) => prevPage - 1); // Go to the previous page
+    }
+  };
+
+  const handleNextPage = () => {
+    if (page < totalPages) {
+      setPage((prevPage) => prevPage + 1); // Go to the next page
+    }
+  };
 
   return (
     <div className='movies-shows-page'>
@@ -76,33 +133,69 @@ const GenrePage = () => {
               wrapperClass='three-dots-loader'
             />
           ) : (
-            <div className='movies-grid'>
-              {data.map((movie) => (
-                <a href='#'>
-                  <div
-                    key={movie.id}
-                    className='movie-card'
-                    style={{
-                      backgroundImage: `url(https://image.tmdb.org/t/p/w200${movie.poster_path})`,
-                    }}
-                  >
-                    <div className='movie-content'>
-                      <span class='rate'>
-                        <i class='fa-solid fa-star'></i>
-                        {movie.vote_average.toFixed(1)}
-                      </span>
-                      <span className='playicon'>
-                        <i className='fas fa-circle-play'></i>
-                      </span>
-                      <div>
-                        <h4 class='title'>{movie.title}</h4>
-                        <h5 class='description'>{movie.overview}</h5>
+            <>
+              <div className='movies-grid'>
+                {data.map((item) => (
+                  <a href='#'>
+                    <div
+                      key={item.id}
+                      className='movie-card'
+                      style={{
+                        backgroundImage: `url(https://image.tmdb.org/t/p/w200${item.poster_path})`,
+                      }}
+                    >
+                      <div className='movie-content'>
+                        <span class='rate'>
+                          <i class='fa-solid fa-star'></i>
+                          {item.vote_average.toFixed(1)}
+                        </span>
+                        <span className='playicon'>
+                          <i className='fas fa-circle-play'></i>
+                        </span>
+                        <div>
+                          <h4 class='title'>{item.title || item.name}</h4>
+                          <h5 class='description'>{item.overview}</h5>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </a>
-              ))}
-            </div>
+                  </a>
+                ))}
+              </div>
+              <nav
+                aria-label='Page navigation'
+                class='d-flex justify-content-center mt-5'
+              >
+                <ul class='pagination'>
+                  <li class='page-item'>
+                    <button
+                      class='page-link cursor-pointer'
+                      type='button'
+                      aria-label='Page Button'
+                      onClick={handlePreviousPage}
+                      disabled={page === 1}
+                    >
+                      <i class='fas fa-angle-double-left'></i>
+                    </button>
+                  </li>
+                  <li class='page-item text-white d-flex align-items-center p-1'>
+                    <span>
+                      Page {page} of {totalPages}
+                    </span>
+                  </li>
+                  <li class='page-item'>
+                    <button
+                      class='page-link cursor-pointer'
+                      type='button'
+                      aria-label='Page Button'
+                      onClick={handleNextPage}
+                      disabled={page === totalPages}
+                    >
+                      <i class='fas fa-angle-double-right'></i>
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+            </>
           )}
         </div>
       </div>
